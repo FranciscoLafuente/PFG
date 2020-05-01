818709055cf9c11d01ac9f236d899a139f1c0cdb
Fran <pacolafuente_5@hotmail.com>
2020-05-01 09:13:21 +0200

    Styles modified

--- frontend/src/pages/TimeLine.vue
+++ frontend/src/pages/TimeLine.vue
@@ -1,25 +1,37 @@
 <template>
   <v-container class="main-container">
-    <v-card class="mx-auto" max-width="400">
-      <v-card dark flat>
-        <v-card-title class="pa-2 lighten-3">
-          <h3 class="title font-weight-light text-center grow">
-            <strong>Scans Performed</strong>
-          </h3>
-        </v-card-title>
-      </v-card>
-      <v-card>
-        <v-timeline dense>
-          <v-timeline-item v-for="(item, i) in data_tl" :key="i" small>
-            <v-row>
-              <v-btn @click="redirectUser(item, i)">
-                <span v-text="item.created"></span>
-              </v-btn>
-            </v-row>
-          </v-timeline-item>
-        </v-timeline>
-      </v-card>
-    </v-card>
+    <v-row dense>
+      <v-col cols="4">
+        <v-card class="mx-auto" max-width="300">
+          <v-card-title>Description</v-card-title>
+          <v-card-text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia corrupti facilis delectus consectetur cum aliquid ad modi! Optio ipsum velit nam! Atque explicabo exercitationem saepe repellat pariatur odio laborum nulla?</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="8" class="scan-col">
+        <div class="cards-container">
+          <v-card class="mx-auto" min-width="400">
+            <v-card color="blue darken-1" flat>
+              <v-card-title class="pa-2 lighten-3">
+                <h3 class="title font-weight-light text-center grow">
+                  <strong class="text-title">Scans Performed</strong>
+                </h3>
+              </v-card-title>
+            </v-card>
+            <v-card>
+              <v-timeline dense>
+                <v-timeline-item v-for="(item, i) in data_tl" :key="i" small>
+                  <v-row>
+                    <v-btn @click="redirectUser(item, i)">
+                      <span v-text="item.created"></span>
+                    </v-btn>
+                  </v-row>
+                </v-timeline-item>
+              </v-timeline>
+            </v-card>
+          </v-card>
+        </div>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -63,5 +75,15 @@ export default {
 <style scoped>
 .main-container {
   margin: 5em;
+}
+
+.scan-col {
+  justify-content: start;
+  text-align: start;
+  display: flex;
+}
+
+.text-title {
+  color: white;
 }
 </style>
