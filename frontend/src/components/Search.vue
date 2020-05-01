67e35ebe8e2508b744533ab70d4030431c58c71e
Fran <pacolafuente_5@hotmail.com>
2020-05-01 17:10:06 +0200

    Some errors solved

--- frontend/src/components/Search.vue
+++ frontend/src/components/Search.vue
@@ -1,7 +1,28 @@
 <template>
-    <div class="container">
-        <v-toolbar dense :elevation=9>
-            <v-text-field hide-details prepend-icon="search" single-line placeholder="Search"></v-text-field>
-        </v-toolbar>
-    </div>
+  <div class="container">
+    <v-toolbar dense :elevation="9">
+      <v-text-field
+        hide-details
+        v-model="lookFor"
+        prepend-icon="search"
+        single-line
+        placeholder="Search"
+        @keypress.enter="search"
+      ></v-text-field>
+    </v-toolbar>
+  </div>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    lookFor: ""
+  }),
+
+  methods: {
+    search() {
+      console.log(this.lookFor);
+    }
+  }
+};
+</script>
